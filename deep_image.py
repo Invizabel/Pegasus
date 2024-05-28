35a7785ea00dae37e5253818b6b523ff57ca8b19
Invizabel <46697708+Invizabel@users.noreply.github.com>
2024-05-28 11:56:10 -0500

    Add files via upload

--- deep_image.py
+++ deep_image.py
@@ -1,66 +1,42 @@
 import argparse
+import math
 import os
 import random
-import re
+import shutil
 from collections import Counter
 from numba import njit
 from PIL import Image
 
 CYAN = "\033[1;36m"
 
+def calculate_distance(gather):
+    distance_array = []
+    for i in range(62):
+        for j in range(0, 63):
+            x1 = gather[i][j]
+            y1 = gather[i][j + 1]
+            x2 = gather[i + 1][j]
+            y2 = gather[i + 1][j + 1]
+            distance = ((x2- x1) ** 2 + (y2 - y1) ** 2) ** (1 / 2)
+            distance_array.append(math.floor(distance))
+
+    return distance_array
+
 @njit
-def numba_train(r, g, b, a, tolerance):
+def numba_transform(r, g, b, a, tolerance):
     count = -1
-    for red in range(0, 255):
-        for green in range(0, 255):
-            for blue in range(0, 255):
-                for alpha in range(0, 255):
+    for red in range(256):
+        for green in range(256):
+            for blue in range(256):
+                for alpha in range(256):
                     count += 1
                     if red - tolerance <= r <= red + tolerance and green - tolerance <= g <= green + tolerance and blue - tolerance <= b <= blue + tolerance and alpha - tolerance <= a <= alpha + tolerance:
                         return count
 
-def generate(my_model):
-    with open(f"{my_model}.csv", "r") as file:
-        data = file.read()
-
-    data = data.replace(";", ",")
-
-    results = re.findall("(\d+),(\d+),(.+),(\d+)", data)
-
-    data_sets = []
-    new_data_set = []
-    new_result = results[0]
-    for result in results:
-        if result[0] == new_result[0] and result[1] == new_result[1]:
-            new_data_set.append(result)
-
-        else:
-            data_sets.append(new_data_set[:])
-            new_data_set = []
-
-        new_result = result
-
-    image = Image.new("RGBA", (64, 64))
-
-    if "low" in my_model:
-        noise = random.randint(0, 10 - 2)
-
-    if "medium" in my_model:
-        noise = random.randint(0, 100 - 2)
-
-    if "large" in my_model:
-        noise = random.randint(0, 1000 - 2)
-
-    for data_set in data_sets:
-        image.putpixel((int(data_set[0][0]), int(data_set[0][1])), (int(data_set[noise][2].split(",")[0].replace("(", "")), int(data_set[noise][2].split(",")[1]), int(data_set[noise][2].split(",")[2]), int(data_set[noise][2].split(",")[3].replace(")", ""))))
-
-    image = image.resize((1920, 1080))
-    image.save(f"{my_model.replace('.csv', '')}.png")
-
 def train(samples, my_model, tolerance):
-    pixels = []
-    models = []
     folders = [my_model]
+    shutil.rmtree("trained_models")
+    os.mkdir("trained_models")
     
     for folder in folders:
         files = os.listdir(folder)
@@ -69,56 +45,72 @@ def train(samples, my_model, tolerance):
 
         print(CYAN + "loading images")
         for file in files:
-            image = Image.open(f"{folder}/{file}").convert("RGBA")
+            image = Image.open(f"{folder}/{file}").convert("L").convert("RGBA")
             image = image.resize((64, 64))
             images.append(image)
 
     for sample in samples:
-        with open(f"model_{sample[0]}_tolerance_{tolerance}.csv", "w") as file:
-            file.write("x,y,pixel,vector\n")
+        hits = []
+        
+        with open(f"trained_models/model_{sample[0]}_t{tolerance}.csv", "w") as file:
+            file.write("distance,weight\n")
 
         temp_models = []
 
         print(CYAN + f"training on {sample[0]}/tolerance-{tolerance}")
 
-        for x in range(0, 64):
-            for y in range(0, 64):
-                pixels = []
-                for image in images:
-                    # process pixels: (x, y)
+        for image in images:
+            gather = []
+            for x in range(0, 64):
+                for y in range(0, 64):
                     r, g, b, a = image.getpixel((x, y))
-                    pixels.append(numba_train(r, g, b, a, tolerance))
+                    gather.append([x, y, numba_transform(r, g, b, a, tolerance)])
 
-                tokens = Counter(pixels).most_common(sample[1])
-                for token in tokens:
-                    temp_models.append([str(x), str(y), str(token[0]), str(token[1])])
+            # transform
+            count = 0
+            temp_var = 0
+            models = []
+            gather.sort(key = lambda x: x[2])
+            for temp in gather:
+                if temp_var != temp[2]:
+                    count += 1
+                    models.append([temp[0], temp[1], count])
 
-        print(CYAN + f"transforming data for {sample[0]}/tolerance-{tolerance}")
-        count = 0
-        temp_var = 0
-        models = []
-        temp_models.sort(key = lambda x: x[2])
-        for temp in temp_models:
-            if temp_var != temp[2]:
-                count += 1
-                models.append([temp[0], temp[1], count, temp[3]])
+                else:
+                     models.append([temp[0], temp[1], count])
 
-            else:
-                 models.append([temp[0], temp[1], count, temp[3]])
+                temp_var = temp[2]
 
-            temp_var = temp[2]
+            models.sort(key = lambda x: (str(x[0]), str(x[1]), str(x[2])))
 
-        models.sort(key = lambda x: (str(x[0]), str(x[1]), str(x[2]), str(x[3])))
-
-        with open(f"model_{sample[0]}_tolerance_{tolerance}.csv", "a") as file:
+            # convert to correct size for distance formula to work
+            init_new_model = []
+            new_model = []
+            old_x = 0
             for model in models:
-                file.write(f"{model[0]},{model[1]},{model[2]},{model[3]}\n")
+                init_new_model.append(model[2])
+
+                if old_x != model[0]:
+                    new_model.append(init_new_model)
+                    init_new_model = []
+
+                old_x = model[0]
+
+            results = calculate_distance(new_model)
+            for result in results:
+                hits.append(result)
+
+        data = Counter(hits).most_common(sample[1])
+        
+        with open(f"trained_models/model_{sample[0]}_t{tolerance}.csv", "a") as file:
+            for i in data:
+                file.write(f"{i[0]},{i[1]}\n")
                 
 def main():
     os.system("clear")
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("-mode", required = True, type = str, choices = ["generate", "train", "verify"])
+    parser.add_argument("-mode", required = True, type = str, choices = ["train"])
     parser.add_argument("-model", required = True, type = str)
     parser.add_argument("-tolerance", required = False, type = int)
     args = parser.parse_args()
@@ -127,9 +119,11 @@ def main():
         generate(args.model)
 
     if args.mode == "train":
-        samples = [["low", 10],
-                   ["medium", 100],
-                   ["large", 1000]]
+        samples = [["low", 100],
+                   ["medium", 1000],
+                   ["large", 10000],
+                   ["xl", 100000],
+                   ["infinite", 1000000]]
 
         tolerances = [1, 2, 4, 8, 16, 32, 64]
 
